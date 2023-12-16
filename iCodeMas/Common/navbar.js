d1919c455f13bc96cf9d207cc19fd88eef275ba7
stef1996janssens <stef.j.1996@outlook.com>
2023-12-16 22:05:48 +0100

    Added new project examples

--- iCodeMas/Common/navbar.js
+++ iCodeMas/Common/navbar.js
@@ -14,7 +14,17 @@ function appendNavbar(){
             <li><a class="nav-link" href="../iCodeMas_03_Ecommerce_promotion/">Promotion</a></li>
             <li><a class="nav-link" href="../iCodeMas_04_Ecommerce_sale_hero/">Sale hero</a></li>
             <li><a class="nav-link" href="../iCodeMas_05_Ecommerce_nav/">Navigationbar</a></li>
+            <li><a class="nav-link" href="../iCodeMas_06_Ecommerce_products/">Products</a></li>
+            <li><a class="nav-link" href="../iCodeMas_07_Ecommerce_filters/">Filters</a></li>
+            <li><a class="nav-link" href="../iCodeMas_08_Ecommerce_shopping_cart/">Shopping cart</a></li>
+            <li><a class="nav-link" href="../iCodeMas_09_Ecommerce_dropdown_filters/">Dropdown filters</a></li>
+            <li><a class="nav-link" href="../iCodeMas_10_Ecommerce_feedback/">Feedback</a></li>
+            <li><a class="nav-link" href="../iCodeMas_11_Ecommerce_gift_cards/">Gift cards</a></li>
             <li><a class="nav-link" href="../iCodeMas_12_Ecommerce_info_cards/">Info cards</a></li>
+            <li><a class="nav-link" href="../iCodeMas_13_Ecommerce_chat/">Chat</a></li>
+            <li><a class="nav-link" href="../iCodeMas_14_Ecommerce_footer/">Footer</a></li>
+            <li><a class="nav-link" href="../iCodeMas_15_Ecommerce_banner_cards/">Banner cards</a></li>
+            <li><a class="nav-link" href="../iCodeMas_16_pager/">Pager</a></li>
         </ul>
     </nav>`
 
